69449a49b4a7734d8d17bfbe46a014787c47d341
Ayush Kumar <ayushk2020@gmail.com>
2023-04-13 02:54:58 -0400

    Logs

--- pricingOracle.py
+++ pricingOracle.py
@@ -2,15 +2,13 @@ import subprocess
 import sys
 from web3 import Web3
 
-# Change this to use your own RPC URL
 web3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com'))
-# AggregatorV3Interface ABI
+print("Connected to RPC")
 abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
-# Price Feed address
 addr = Web3.to_checksum_address('0xa07703e5c2ed1516107c7c72a494493dcb99c676')
-# Set up contract instance
+
 contract = web3.eth.contract(address=addr, abi=abi)
-# Make call to latestRoundData()
+
+print("Getting price...")
 latestData = contract.functions.latestRoundData().call()
-# Print the latest price
 print(latestData[1]/10**8)
